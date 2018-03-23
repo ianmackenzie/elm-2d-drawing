f95903ae28142629005ce7620a3074c971b2ca54
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-03-23 09:13:48 -0400

    Start adding Attributes module
    A bit simpler than having to import separate Stroke, Fill, Dot etc., modules, and more amenable to being imported unqualified if that is preferred

---
+++ src/Drawing2d/Attributes.elm
@@ -0,0 +1,12 @@
+module Drawing2d.Attributes
+    exposing
+        ( fillColor
+        , noFill
+        , blackFill
+        , whiteFill
+        , strokeColor
+        , noStroke
+        , blackStroke
+        , whiteStroke
+        , dotRadius
+        )
