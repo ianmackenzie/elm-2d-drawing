5b0524787f468ec7d0586307b081b7bed3dd27ab
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-03-24 17:19:03 -0400

    Fill in rest of initial API

--- src/Drawing2d.elm
+++ src/Drawing2d.elm
@@ -4,42 +4,57 @@ module Drawing2d
         , Element
           --, arrow
           --, arrowWith
-          --, circle
-          --, cubicSpline
-          --, ellipse
-          --, ellipticalArc
+        , arc
+        , arcWith
+        , circle
+        , circleWith
+        , cubicSpline
+        , cubicSplineWith
         , dot
         , dotWith
-          --, polygon
-          --, polyline
-          --, quadraticSpline
+        , ellipse
+        , ellipseWith
+        , ellipticalArc
+        , ellipticalArcWith
         , empty
-          --, arc
         , group
         , groupWith
         , lineSegment
         , lineSegmentWith
         , mirrorAcross
         , placeIn
+        , polygon
+        , polygonWith
+        , polyline
+        , polylineWith
+        , quadraticSpline
+        , quadraticSplineWith
         , relativeTo
-          --, scaleAbout
+        , scaleAbout
           --, text
         , toHtml
-          --, translateBy
+        , translateBy
         , triangle
         , triangleWith
         )
 
+import Arc2d exposing (Arc2d)
 import Axis2d exposing (Axis2d)
 import BoundingBox2d exposing (BoundingBox2d)
 import Circle2d exposing (Circle2d)
+import CubicSpline2d exposing (CubicSpline2d)
 import Drawing2d.Internal as Internal exposing (applyAttributes, defaultContext, svgAttributes)
+import Ellipse2d exposing (Ellipse2d)
+import EllipticalArc2d exposing (EllipticalArc2d)
 import Frame2d exposing (Frame2d)
 import Geometry.Svg as Svg
 import Html exposing (Html)
 import Html.Attributes
 import LineSegment2d exposing (LineSegment2d)
 import Point2d exposing (Point2d)
+import Polygon2d exposing (Polygon2d)
+import Polyline2d exposing (Polyline2d)
+import QuadraticSpline2d exposing (QuadraticSpline2d)
 import Svg exposing (Svg)
 import Svg.Attributes
 import Triangle2d exposing (Triangle2d)
@@ -70,6 +85,9 @@ toSvgElement parentContext element =
 
         Internal.PlaceIn frame element ->
             Svg.placeIn frame (toSvgElement parentContext element)
+
+        Internal.ScaleAbout point scale element ->
+            Svg.scaleAbout point scale (toSvgElement parentContext element)
 
         Internal.Arrow attributes basePoint length direction ->
             let
@@ -124,6 +142,30 @@ toSvgElement parentContext element =
             in
             Svg.circle2d (svgAttributes attributes)
                 (Circle2d.withRadius localContext.dotRadius point)
+
+        Internal.Arc attributes arc ->
+            Svg.arc2d (svgAttributes attributes) arc
+
+        Internal.QuadraticSpline attributes quadraticSpline ->
+            Svg.quadraticSpline2d (svgAttributes attributes) quadraticSpline
+
+        Internal.CubicSpline attributes cubicSpline ->
+            Svg.cubicSpline2d (svgAttributes attributes) cubicSpline
+
+        Internal.Circle attributes circle ->
+            Svg.circle2d (svgAttributes attributes) circle
+
+        Internal.Ellipse attributes ellipse ->
+            Svg.ellipse2d (svgAttributes attributes) ellipse
+
+        Internal.EllipticalArc attributes ellipticalArc ->
+            Svg.ellipticalArc2d (svgAttributes attributes) ellipticalArc
+
+        Internal.Polyline attributes polyline ->
+            Svg.polyline2d (svgAttributes attributes) polyline
+
+        Internal.Polygon attributes polygon ->
+            Svg.polygon2d (svgAttributes attributes) polygon
 
 
 toHtml : BoundingBox2d -> List (Attribute msg) -> List (Element msg) -> Html msg
@@ -232,6 +274,96 @@ dotWith attributes point =
     Internal.Dot attributes point
 
 
+translateBy : Vector2d -> Element msg -> Element msg
+translateBy displacement element =
+    placeIn (Frame2d.translateBy displacement Frame2d.xy) element
+
+
+scaleAbout : Point2d -> Float -> Element msg -> Element msg
+scaleAbout point scale element =
+    Internal.ScaleAbout point scale element
+
+
 mirrorAcross : Axis2d -> Element msg -> Element msg
 mirrorAcross axis element =
     placeIn (Frame2d.mirrorAcross axis Frame2d.xy) element
+
+
+arc : Arc2d -> Element msg
+arc =
+    arcWith []
+
+
+arcWith : List (Attribute msg) -> Arc2d -> Element msg
+arcWith attributes arc =
+    Internal.Arc attributes arc
+
+
+quadraticSpline : QuadraticSpline2d -> Element msg
+quadraticSpline =
+    quadraticSplineWith []
+
+
+quadraticSplineWith : List (Attribute msg) -> QuadraticSpline2d -> Element msg
+quadraticSplineWith attributes quadraticSpline =
+    Internal.QuadraticSpline attributes quadraticSpline
+
+
+cubicSpline : CubicSpline2d -> Element msg
+cubicSpline =
+    cubicSplineWith []
+
+
+cubicSplineWith : List (Attribute msg) -> CubicSpline2d -> Element msg
+cubicSplineWith attributes cubicSpline =
+    Internal.CubicSpline attributes cubicSpline
+
+
+polyline : Polyline2d -> Element msg
+polyline =
+    polylineWith []
+
+
+polylineWith : List (Attribute msg) -> Polyline2d -> Element msg
+polylineWith attributes polyline =
+    Internal.Polyline attributes polyline
+
+
+polygon : Polygon2d -> Element msg
+polygon =
+    polygonWith []
+
+
+polygonWith : List (Attribute msg) -> Polygon2d -> Element msg
+polygonWith attributes polygon =
+    Internal.Polygon attributes polygon
+
+
+circle : Circle2d -> Element msg
+circle =
+    circleWith []
+
+
+circleWith : List (Attribute msg) -> Circle2d -> Element msg
+circleWith attributes circle =
+    Internal.Circle attributes circle
+
+
+ellipticalArc : EllipticalArc2d -> Element msg
+ellipticalArc =
+    ellipticalArcWith []
+
+
+ellipticalArcWith : List (Attribute msg) -> EllipticalArc2d -> Element msg
+ellipticalArcWith attributes ellipticalArc =
+    Internal.EllipticalArc attributes ellipticalArc
+
+
+ellipse : Ellipse2d -> Element msg
+ellipse =
+    ellipseWith []
+
+
+ellipseWith : List (Attribute msg) -> Ellipse2d -> Element msg
+ellipseWith attributes ellipse =
+    Internal.Ellipse attributes ellipse
--- src/Drawing2d/Internal.elm
+++ src/Drawing2d/Internal.elm
@@ -1,10 +1,18 @@
 module Drawing2d.Internal exposing (..)
 
+import Arc2d exposing (Arc2d)
+import Circle2d exposing (Circle2d)
 import Color exposing (Color)
+import CubicSpline2d exposing (CubicSpline2d)
 import Direction2d as Direction2d exposing (Direction2d)
+import Ellipse2d exposing (Ellipse2d)
+import EllipticalArc2d exposing (EllipticalArc2d)
 import Frame2d as Frame2d exposing (Frame2d)
 import LineSegment2d exposing (LineSegment2d)
 import Point2d as Point2d exposing (Point2d)
+import Polygon2d exposing (Polygon2d)
+import Polyline2d exposing (Polyline2d)
+import QuadraticSpline2d exposing (QuadraticSpline2d)
 import Svg
 import Svg.Attributes
 import Triangle2d exposing (Triangle2d)
@@ -36,10 +44,19 @@ type Element msg
     = Empty
     | Group (List (Attribute msg)) (List (Element msg))
     | PlaceIn Frame2d (Element msg)
+    | ScaleAbout Point2d Float (Element msg)
     | Arrow (List (Attribute msg)) Point2d Float Direction2d
     | LineSegment (List (Attribute msg)) LineSegment2d
     | Triangle (List (Attribute msg)) Triangle2d
     | Dot (List (Attribute msg)) Point2d
+    | Arc (List (Attribute msg)) Arc2d
+    | CubicSpline (List (Attribute msg)) CubicSpline2d
+    | QuadraticSpline (List (Attribute msg)) QuadraticSpline2d
+    | Polyline (List (Attribute msg)) Polyline2d
+    | Polygon (List (Attribute msg)) Polygon2d
+    | Circle (List (Attribute msg)) Circle2d
+    | Ellipse (List (Attribute msg)) Ellipse2d
+    | EllipticalArc (List (Attribute msg)) EllipticalArc2d
 
 
 type alias Context =
