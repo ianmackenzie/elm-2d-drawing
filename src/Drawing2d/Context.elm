000c06aff5cac5a55ee93542677b1caac33e65f6
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-05-16 12:20:02 -0400

    Update Context to 0.19 and units/coordinates

--- src/Drawing2d/Context.elm
+++ src/Drawing2d/Context.elm
@@ -1,19 +1,24 @@
-module Drawing2d.Context exposing (Context, init)
+module Drawing2d.Context exposing (Context, SvgCoordinates, init)
 
 import BoundingBox2d exposing (BoundingBox2d)
 import Drawing2d.Border as Border exposing (BorderPosition)
 import Frame2d exposing (Frame2d)
+import Quantity exposing (Quantity)
 
 
-type alias Context =
-    { dotRadius : Float
+type SvgCoordinates
+    = SvgCoordinates
+
+
+type alias Context units coordinates =
+    { dotRadius : Quantity Float units
     , bordersEnabled : Bool
     , borderPosition : BorderPosition
-    , placementFrame : Frame2d
+    , placementFrame : Frame2d units SvgCoordinates coordinates
     }
 
 
-init : BoundingBox2d -> Context
+init : BoundingBox2d units coordinates -> Context units coordinates
 init renderBounds =
     let
         { minX, maxY } =
@@ -22,7 +27,7 @@ init renderBounds =
         topLeftFrame =
             Frame2d.atCoordinates ( minX, maxY ) |> Frame2d.reverseY
     in
-    { dotRadius = 0
+    { dotRadius = Quantity.zero
     , bordersEnabled = False
     , borderPosition = Border.centered
     , placementFrame = Frame2d.xy |> Frame2d.relativeTo topLeftFrame
