cf4d25a0219ea3f7082b3c1ce63c2a7efa2ff177
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-11-27 17:02:06 -0500

    Tweak image cursor example layout

--- examples/ImageCursor.elm
+++ examples/ImageCursor.elm
@@ -12,13 +12,13 @@ main : Html Never
 main =
     let
         rect1 =
-            Rectangle2d.from (Point2d.pixels 100 700) (Point2d.pixels 700 500)
+            Rectangle2d.from (Point2d.pixels 100 400) (Point2d.pixels 700 600)
 
         rect2 =
             Rectangle2d.from (Point2d.pixels 100 100) (Point2d.pixels 700 300)
     in
     Drawing2d.draw
-        { viewBox = Rectangle2d.from Point2d.origin (Point2d.pixels 800 800)
+        { viewBox = Rectangle2d.from Point2d.origin (Point2d.pixels 800 700)
         , attributes = [ Drawing2d.whiteText, Drawing2d.anchorAtCenter, Drawing2d.fontSize (Pixels.float 96) ]
         , background = Drawing2d.noBackground
         , entities =
